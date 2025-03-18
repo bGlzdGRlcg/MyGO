cbb29a718c4f9ced792893e87ec6f9c5bba08781
bGlzdGRlcg <listder@outlook.com>
2025-03-18 22:12:20 +0800

    fix: can't fetch public mention

--- bots/bots.go
+++ bots/bots.go
@@ -38,13 +38,13 @@ func Start() {
 
 	for event := range events {
 		switch e := event.(type) {
-		case *mastodon.UpdateEvent:
-			fmt.Printf("收到新嘟文: %s\n", e.Status.Content)
-			Reply(c, e.Status)
 		case *mastodon.NotificationEvent:
 			fmt.Printf("收到新通知: %s\n", e.Notification.Type)
 			if e.Notification.Type == "follow" {
 				c.AccountFollow(ctx, e.Notification.Account.ID)
+			} else if e.Notification.Type == "mention" {
+				fmt.Printf("收到新嘟文: %s\n", e.Notification.Status.Content)
+				Reply(c, e.Notification.Status)
 			}
 		case *mastodon.DeleteEvent:
 			fmt.Printf("嘟文被删除: %s\n", e.ID)
